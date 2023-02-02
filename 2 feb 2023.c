2746fa4d57f6a1ae35f611cfbb36ec13ecd2a533
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-02-02 20:27:39 +0530

    Update 2 feb 2023.c

--- 2 feb 2023.c
+++ 2 feb 2023.c
@@ -1,43 +1,3 @@
-//First Program
-#include <stdio.h>
-int main() {
-    printf("Hello, World");
-    return 0;
-}
-
-//________________________________________________________________
-
-//Prime number
-#include <math.h>
-#include <stdio.h>
-int main ()
-{
-    int n, i, flag = 1;
-    printf ("Enter a number: \n");
-    scanf ("%d", &n);
-    for (i = 2; i <= sqrt(n); i++) {
-        if (n % i == 0) {
-            flag = 0;
-            break;
-        }
-    }
-
-    if (n <= 1){
-        flag = 0;
-    }
-
-    if (flag == 1){
-        printf("%d is a prime number",n);
-    }
-    else{
-        printf("%d is not a prime number",n);
-    }
-
-    return 0;
-}
-
-//________________________________________________________________________
-
 //Char
 #include <stdio.h>
 int main(){
@@ -46,5 +6,3 @@ int main(){
     printf("%d \n",chr);
     return 0;
 }
-
-//________________________________________________________________________
