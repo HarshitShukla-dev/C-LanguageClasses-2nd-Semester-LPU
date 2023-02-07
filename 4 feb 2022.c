0fe685855defdea36091efa4dfa069216474e36b
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-02-07 22:02:16 +0530

    Add files via upload

---
+++ 4 feb 2022.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    float a = 10;
+
+    printf("%d",a);
+
+    return 0;
+}
+
+//________________________________________________________________
+
+#include <stdio.h>
+int main()
+{
+    int i = -3;
+    int k = i % 2;
+
+    printf("%d\n",k);
+
+    return 0;
+}
+
+//________________________________________________________________
+
+#include <stdio.h>
+int main()
+{
+    int i = 3;
+    int l = i / -2;
+    int k = i % 2;
+
+    printf("%d %d\n",l,k);
+
+    return 0;
+}
+
+//________________________________________________________________
+
+#include <stdio.h>
+int main()
+{
+    int x = 5.3 % 2;
+
+    printf("%d\n",x);
+
+    return 0;
+}
