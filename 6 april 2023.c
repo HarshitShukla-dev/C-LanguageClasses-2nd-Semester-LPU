54bba38524eaf7aa314f58b7096150a08716db1c
Hershit-shukla <109151752+Hershit-shukla@users.noreply.github.com>
2023-04-06 09:48:49 +0530

    Structure in C

--- 6 april 2023.c
+++ 6 april 2023.c
@@ -19,3 +19,31 @@ int main(){
 
 //_________________________________________
 
+#include <stdio.h>
+struct student{
+    char name[30];
+    int age;
+    int mobile;
+    int regID;
+    float cgpa;
+};
+int main(){
+    struct student myStudent;
+    printf("Enter the name: ");
+    scanf("%s", myStudent.name);
+    printf("Enter the age: ");
+    scanf("%d",&myStudent.age);
+    printf("Enter the mobile: ");
+    scanf("%d",&myStudent.mobile);
+    printf("Enter the regID: ");
+    scanf("%d",&myStudent.regID);
+    printf("Enter the cgpa: ");
+    scanf("%f",&myStudent.cgpa);
+
+    printf("Name: %s, age: %d, mobile no.: %d, reg id: %d, cgpa: %.2f",myStudent.name,myStudent.age,myStudent.mobile,myStudent.regID,myStudent.cgpa);
+    printf("\nSize of structure: %d",sizeof(myStudent));
+    return 0;
+
+}
+
+//_________________________________________
