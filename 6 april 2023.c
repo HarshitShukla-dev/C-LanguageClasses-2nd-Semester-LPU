db69e2385fd234ef1360afebfe00e06597417aae
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-06 10:37:59 +0530

    6 april

--- 6 april 2023.c
+++ 6 april 2023.c
@@ -67,3 +67,34 @@ int main()
     printf("Data.i : %d\n", Data.i);
     return 0;
 }
+
+//_________________________________________
+
+#include <stdio.h>
+union student
+{
+    char name[20];
+    int age;
+    int mobile;
+    int regid;
+    float cgpa;
+};
+int main()
+{
+    union student st1;
+
+    printf("Enter the name: ");
+    scanf("%s", &st1.name);
+    printf("Enter the age: ");
+    scanf("%d", &st1.age);
+    printf("Enter the mobile number: ");
+    scanf("%d", &st1.mobile);
+    printf("Enter the regid: ");
+    scanf("%d", &st1.regid);
+    printf("Enter the cgpa: ");
+    scanf("%f", &st1.cgpa);
+    printf("%s %d %d %f", st1.name, st1.age, st1.mobile, st1.cgpa);
+    printf("\nThe size of variable str1 is: %d", sizeof(st1));
+}
+
+//________________________________________
