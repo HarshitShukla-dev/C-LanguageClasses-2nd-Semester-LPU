dc936bc5044268682aae1ceced79c12f29b81956
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-12 04:48:31 +0000

    Wednesday, April 12, 2023, 10:18 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,12 @@
+//Pointers in C
+
 #include <stdio.h>
 int main(){
     int x = 10, *p;
     p = &x;
     printf("%u\n", p);
     printf("%d\n",*p);
+    printf("%p\n",*p);
+    printf("%p\n",p);
     return 0;
 }
