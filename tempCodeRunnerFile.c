0f6aa408507cacb6af1dfc995c3f8ec2ca4c3f6b
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-12 04:58:09 +0000

    Wednesday, April 12, 2023, 10:28 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -5,10 +5,10 @@ int main()
     char *pch;
     float *pfloat;
     long *pdouble;
-    printf("\nsize of int pointer=%d",sizeof(pnum))
-    printf("\nsize of char pointer=%d",sizeof(pch))
-    printf("\nsize of float pointer=%f",sizeof(pfloat))
-    printf("\nsize of double pointer=%d",sizeof(pdouble))
+    printf("\nsize of int pointer=%d",sizeof(pnum));
+    printf("\nsize of char pointer=%d",sizeof(pch));
+    printf("\nsize of float pointer=%f",sizeof(pfloat));
+        printf("\nsize of double pointer=%d",sizeof(pdouble));
 
     return 0;
 }
