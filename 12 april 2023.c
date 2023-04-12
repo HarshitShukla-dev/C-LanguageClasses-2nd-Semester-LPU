92cd6f8b414e3be9dec007ed9dd8899efd5cebe4
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-12 04:50:41 +0000

    Wednesday, April 12, 2023, 10:20 AM India Standard Time

--- 12 april 2023.c
+++ 12 april 2023.c
@@ -1,12 +1,22 @@
-//Pointers in C
+// Pointers in C
 
 #include <stdio.h>
-int main(){
+int main()
+{
     int x = 10, *p;
     p = &x;
     printf("%u\n", p);
-    printf("%d\n",*p);
-    printf("%p\n",*p);
-    printf("%p\n",p);
+    printf("%d\n", *p);
+    printf("%p\n", *p);
+    printf("%p\n", p);
+    return 0;
+}
+
+//______________________________
+
+#include <stdio.h>
+int main()
+{
+
     return 0;
 }
