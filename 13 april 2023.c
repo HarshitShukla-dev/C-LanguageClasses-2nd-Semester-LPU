5af2f8d7826a02cc0ece9721f66bb237d4355e05
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-13 05:14:09 +0000

    Thursday, April 13, 2023, 10:44 AM India Standard Time

--- 13 april 2023.c
+++ 13 april 2023.c
@@ -24,3 +24,16 @@ int main()
 
 //____________________________________________
 
+#include <stdio.h>
+
+int main() {
+    int x = 10, y = 20;
+    int *p, *q;
+
+    p = &x;
+    q = &y;
+
+    (*p > *q) ? printf("%d is greater than %d\n", *p, *q) : printf("%d is greater than %d\n", *q, *p);
+
+    return 0;
+}
