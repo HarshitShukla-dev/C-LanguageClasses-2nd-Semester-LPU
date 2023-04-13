77e933cfcc3c71ccd64abaca006a409e22b16525
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-13 04:56:17 +0000

    Thursday, April 13, 2023, 10:26 AM India Standard Time

--- 13 april 2023.c
+++ 13 april 2023.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9};
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int *p1, *p2;
     p1 = arr;
-    p1++;
-    printf("%d",*p1);
+    p1++;                // p1 will point towards next memory location
+    printf("\n%d", *p1); // 2 will be printed
     p1--;
-    printf("%d",*p1);
+    printf("\n%d", *p1);
+    p1 = p1 + 2; // addding a constant to pointer (p1 will point towards
+    printf("\n%d", *p1);
+    p1 = p1 - 2;
+    printf("\n%d", *p1); // 1 will be printed
+    p2 = &arr[4];
+    printf("\n%d", p2 - p1); // returns 4
+    while (p1 <= p2)
+    {
+        printf("\n%d", *p1);
+        p1++;
+    }
     return 0;
-}
-
-//___________________________
-
-#include <stdio.h>
-int main(){
-    
 }
