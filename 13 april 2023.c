ab801b63115de0339f2245dd9e188482eef24e34
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-13 05:23:51 +0000

    Thursday, April 13, 2023, 10:53 AM India Standard Time

--- 13 april 2023.c
+++ 13 april 2023.c
@@ -26,7 +26,8 @@ int main()
 
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int x = 10, y = 20;
     int *p, *q;
 
@@ -39,3 +40,20 @@ int main() {
 }
 
 //____________________________________________
+
+#include <stdio.h>
+int main()
+{
+    int a[] = {1, 2, 3, 4};
+    int *p1 = a, i;
+    int *p2 = &a[2];
+    p2--;
+    *(p2 - 1) = 90;
+    p1 = p2;
+    *p1 = 100;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+    return 0;
+}
