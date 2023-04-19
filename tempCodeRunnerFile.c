d44f33e251d892f18b7f66414d255d02370f83dd
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 05:14:48 +0000

    Wednesday, April 19, 2023, 10:44 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-int main()
-{
+int main(){
     FILE *file;
-    if (file = fopen("FileHandling01.txt", "a"))
-    {
-        if (fputs("He was a great personality.",file) >= 0)
-        {
-            printf("File opened successfully in append mode");
-        }
-        else
-        {
-            printf("Error in opening file");
-        }
+    if (file = fopen("FileHandling01.txt", "r+")){
+        printf("File opened successfully in read and write mode");
+    }
+    else{
+        printf("Error in opening file");
     }
     fclose(file);
     return 0;
