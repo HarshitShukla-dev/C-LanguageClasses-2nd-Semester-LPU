37d35699683ffd667b203d0375b985920abcbc25
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 04:59:34 +0000

    Wednesday, April 19, 2023, 10:29 AM India Standard Time

--- 19 april 2023.c
+++ 19 april 2023.c
@@ -21,3 +21,18 @@ int main()
 
 //_____________________________________________
 
+#include <stdio.h>
+int main()
+{
+    FILE *file;
+    if (file = fopen("hello.txt", "r"))
+    {
+        printf("File opened successfully in read mode");
+    }
+    else
+    {
+        printf("Error in opening file");
+    }
+    fclose(file);
+    return 0;
+}
