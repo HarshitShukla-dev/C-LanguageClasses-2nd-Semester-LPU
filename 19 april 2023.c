8c4d0e80a46a553700a7610a219db6955ef15182
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 04:50:37 +0000

    Wednesday, April 19, 2023, 10:20 AM India Standard Time

--- 19 april 2023.c
+++ 19 april 2023.c
@@ -1,1 +1,14 @@
 //File Handling
+
+#include <stdio.h>
+int main(){
+    FILE * file;
+    if (file = fopen("/hello.txt","w")){
+        if (fputs("Swami Vivekanand....", file) >= 0){
+            printf("File opened successfully in write mode");
+        }
+        else{
+            
+        }
+    }
+}
