0062889d3a91154733242a23b8dd327c6592d059
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 04:50:53 +0000

    Wednesday, April 19, 2023, 10:20 AM India Standard Time

--- 19 april 2023.c
+++ 19 april 2023.c
@@ -1,14 +1,19 @@
-//File Handling
+// File Handling
 
 #include <stdio.h>
-int main(){
-    FILE * file;
-    if (file = fopen("/hello.txt","w")){
-        if (fputs("Swami Vivekanand....", file) >= 0){
+int main()
+{
+    FILE *file;
+    if (file = fopen("/hello.txt", "w"))
+    {
+        if (fputs("Swami Vivekanand....", file) >= 0)
+        {
             printf("File opened successfully in write mode");
         }
-        else{
-            
+        else
+        {
+            printf("Error in opening file");
         }
     }
+    return 0;
 }
