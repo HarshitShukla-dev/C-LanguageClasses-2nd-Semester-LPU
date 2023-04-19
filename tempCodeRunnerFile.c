0e5899fed393af54ea6788c4c380bc491ab88b31
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 05:22:17 +0000

    Wednesday, April 19, 2023, 10:52 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(){
     FILE *file;
-    if (file = fopen("FileHandling01.txt", "r+")){
-        printf("File opened successfully in read and write mode");
+    if (file = fopen("FileHandling02.txt", "r+")){
+        fprintf("Home Address: Bangarmau, Unnao, Uttar-Pradesh India 209868",file);
+        fprintf("Current Address: C101 - Bh6, Lovely Professional University",file);
+        printf("Done ...........");
     }
     else{
         printf("Error in opening file");
