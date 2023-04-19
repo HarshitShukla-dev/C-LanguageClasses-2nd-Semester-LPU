421449c8272e3b7d31466cdf32ed3775052057c2
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 04:59:47 +0000

    Wednesday, April 19, 2023, 10:29 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,16 +2,13 @@
 int main()
 {
     FILE *file;
-    if (file = fopen("hello.txt", "w"))
+    if (file = fopen("hello.txt", "r"))
     {
-        if (fputs("Swami Vivekanand....", file) >= 0)
-        {
-            printf("File opened successfully in write mode");
-        }
-        else
-        {
-            printf("Error in opening file");
-        }
+        printf("File opened successfully in read mode");
+    }
+    else
+    {
+        printf("Error in opening file");
     }
     fclose(file);
     return 0;
