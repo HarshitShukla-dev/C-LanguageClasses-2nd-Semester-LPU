9e02ae34b3944cc436d35617fabdd4bc9044782e
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 05:55:11 +0000

    Wednesday, April 19, 2023, 11:25 AM India Standard Time

--- 19 april 2023.c
+++ 19 april 2023.c
@@ -121,11 +121,13 @@ int main()
 //_____________________________________________
 
 #include <stdio.h>
-int main(){
+int main()
+{
     FILE *file;
     char str[500];
-    if (file = fopen("FileHandling01.txt", "r")){
-        printf("%s",fgets(str,10,file));
+    if (file = fopen("FileHandling01.txt", "r"))
+    {
+        printf("%s", fgets(str, 10, file));
     }
     fclose(file);
     return 0;
