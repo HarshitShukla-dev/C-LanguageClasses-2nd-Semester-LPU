5039c5e4f40e5ae769f6b75dcd7b5b6761ea8918
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 05:23:20 +0000

    Wednesday, April 19, 2023, 10:53 AM India Standard Time

--- 19 april 2023.c
+++ 19 april 2023.c
@@ -75,3 +75,17 @@ int main(){
 
 //_____________________________________________
 
+#include <stdio.h>
+int main(){
+    FILE *file;
+    if (file = fopen("FileHandling01.txt", "r+")){
+        fputs("Home Address: Bangarmau, Unnao, Uttar-Pradesh India 209868",file);
+        fputs("Current Address: C101 - Bh6, Lovely Professional University",file);
+        printf("Done ...........");
+    }
+    else{
+        printf("Error in opening file");
+    }
+    fclose(file);
+    return 0;
+}
