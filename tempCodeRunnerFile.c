4a138b5770f938de31bac782745f5665a29b52a8
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-19 05:22:56 +0000

    Wednesday, April 19, 2023, 10:52 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,8 +2,8 @@
 int main(){
     FILE *file;
     if (file = fopen("FileHandling02.txt", "r+")){
-        fprintf("Home Address: Bangarmau, Unnao, Uttar-Pradesh India 209868",file);
-        fprintf("Current Address: C101 - Bh6, Lovely Professional University",file);
+        fputs("Home Address: Bangarmau, Unnao, Uttar-Pradesh India 209868",file);
+        fputs("Current Address: C101 - Bh6, Lovely Professional University",file);
         printf("Done ...........");
     }
     else{
