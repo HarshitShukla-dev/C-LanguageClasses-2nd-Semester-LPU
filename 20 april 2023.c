e4a29d0f2ab13b0bf82ed5ca2638849b47720b2f
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-20 05:17:14 +0000

    Thursday, April 20, 2023, 10:47 AM India Standard Time

--- 20 april 2023.c
+++ 20 april 2023.c
@@ -88,3 +88,18 @@ int main()
     }
     return 0;
 }
+
+//_____________________________________
+
+#include <stdio.h>
+int main()
+{
+    char Name[30];
+    char Address[50];
+    printf("Enter your Name : ");
+    scanf("%s", Name);
+    printf("Enter your Address : ");
+    scanf("%s", Address);
+    printf("\nYour Name is %s and you live at %s", Name, Address);
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     char Name[30];
     char Address[50];
     printf("Enter your Name : ");
-    scanf("%s",Name);
+    scanf("%s", Name);
     printf("Enter your Address : ");
-    scanf("%s",Address);
-    printf("Your Name is %s and you live at %s",Name,Address);
+    scanf("%s", Address);
+    printf("\nYour Name is %s and you live at %s", Name, Address);
     return 0;
 }
