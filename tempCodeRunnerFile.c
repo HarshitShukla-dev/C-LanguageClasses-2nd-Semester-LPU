a7d70e502a86bb3dbad24bcb69da2e6989f8bd41
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-20 04:49:42 +0000

    Thursday, April 20, 2023, 10:19 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    FILE *file;
-    if (file = fopen("FileHandling01.txt", "w"))
+    struct student
     {
-        fputc('H', file);
-        printf("DONE......");
+        int id;
+        char name[20];
+        double mob;
+    }s1,s2;
+
+    FILE *ptr;
+    ptr = fopen("FileHandling02.bin","wb+");
+    if(ptr == NULL){
+        printf("File not found");
     }
-    fclose(file);
-    return 0;
+    else{
+        printf("File created..");
+        printf("Enter id : ");
+        scanf("%d",&s1.id);
+        printf("Enter name : ");
+        scanf("%s",s1.name);
+        printf("Enter mob : ");
+        scanf("%lf",&s1.mob);
+        fwrite(&s1,sizeof(s1),1,ptr);
+        fclose(ptr);
+        printf("File saved..");
+    }
+    
+    FILE *file;
 }
