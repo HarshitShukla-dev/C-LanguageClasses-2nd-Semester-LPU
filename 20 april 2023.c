c04d0f40a29d4852fef72ee272ed62f629ae8960
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-20 04:12:45 +0000

    Thursday, April 20, 2023, 9:42 AM India Standard Time

--- 20 april 2023.c
+++ 20 april 2023.c
@@ -17,3 +17,17 @@ int main()
     fclose(fp);
     return 0;
 }
+
+//_____________________________________
+
+#include <stdio.h>
+int main(){
+    FILE *file;
+    if(file=fopen("FileHandling01.txt","w")){
+        if(fprintf(file,"He was a great personality.....")>=0){
+            printf("Successfull");
+        }
+    }
+    fclose(file);
+    return 0;
+}
