944e17e03b9bd19f91bc3a2325dd3fd9f0bc2bca
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-20 05:16:14 +0000

    Thursday, April 20, 2023, 10:46 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,36 +1,11 @@
 #include <stdio.h>
-int main()
-{
-    struct student
-    {
-        int id;
-        char name[20];
-        double mob;
-    } s1, s2;
-
-    FILE *ptr;
-    ptr = fopen("FileHandling02.bin", "wb+");
-    if (ptr == NULL)
-    {
-        printf("File not found");
-    }
-    else
-    {
-        printf("File created..");
-        printf("\nEnter id : ");
-        scanf("%d", &s1.id);
-        printf("Enter name : ");
-        scanf("%s", s1.name);
-        printf("Enter mob : ");
-        scanf("%lf", &s1.mob);
-
-        fwrite(&s1, sizeof(s1), 1, ptr);
-        fseek(ptr, 0, SEEK_SET);
-        fread(&s2, sizeof(s2), 1, ptr);
-        printf("\n\nYour input :");
-        printf("\nID: %d \nName: %s \nMobile Number: %.2lf", s2.id, s2.name, s2.mob);
-        fclose(ptr);
-        printf("\n\nFile saved..");
-    }
+int main(){
+    char Name[30];
+    char Address[50];
+    printf("Enter your Name : ");
+    scanf("%s",Name);
+    printf("Enter your Address : ");
+    scanf("%s",Address);
+    printf("Your Name is %s and you live at %s",Name,Address);
     return 0;
 }
