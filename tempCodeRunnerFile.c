66c76bd9d0540bcce89847a11ae3d3b139483ba7
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-20 03:58:01 +0000

    Thursday, April 20, 2023, 9:28 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-int main(){
-    FILE * fp;
-    fp = fopen("FileHandling01.txt","r");
-    if (!fp){
+int main()
+{
+    FILE *fp;
+    fp = fopen("FileHandling01.txt", "r");
+    if (!fp)
+    {
         printf("Error...!!!");
         return 0;
     }
     fseek(fp, 6, 0);
     char ch;
-    while (ch = fgetc(fp)){
+    while (ch = fgetc(fp))
+    {
         printf("%c", ch);
-        }
-        fclose(fp);
-        return 0;
+    }
+    fclose(fp);
+    return 0;
 }
