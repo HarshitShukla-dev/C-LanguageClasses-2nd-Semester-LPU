230a389f60d443977aebe9502301fb07fd85d5ed
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-26 05:16:37 +0000

    Wednesday, April 26, 2023, 10:46 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 void main()
 {
-    char country[] = "INDIA";
-    char *ptr;
-    ptr = country;
-    while (*ptr != '\0')
-    {
-        printf("%c", *ptr);
-        ptr++;
-    }
+    // strcopy() function
+    char ori[20], dup[20];
+    char *s;
+    printf("Enter your name: ");
+    gets(ori);
+    printf("Enter your duplicate name: ");
+    gets(dup);
+    s = strcopy(ori, dup);
+    printf("\nOriginal string is: %s", ori);
+    printf("\nDuplicate string is: %s", dup);
+    printf("\nValue of s is: %s", s);
 }
