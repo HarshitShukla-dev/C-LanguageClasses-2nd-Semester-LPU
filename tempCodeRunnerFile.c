d1b9f683919d1cf4ac9d755911b99c2bd77e3d9d
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-26 05:00:05 +0000

    Wednesday, April 26, 2023, 10:30 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-void main()
-{
-    char name[] = ("Lovely Professional University");
-    int i = 0;
-    while (name[i] != '\0')
-    {
-        printf("%c", name[i]);
-        i++;
-    }
+void main(){
+    char ary[] = "Discovery Channel";
+    printf("%s",ary);
 }
