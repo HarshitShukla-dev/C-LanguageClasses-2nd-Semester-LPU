aa249d9ce62f8df7a34d90879b545ae187365f92
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-26 05:18:20 +0000

    Wednesday, April 26, 2023, 10:48 AM India Standard Time

--- 26 april 2023.c
+++ 26 april 2023.c
@@ -45,3 +45,22 @@ void main()
         ptr++;
     }
 }
+
+//_________________________________
+
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    // strcopy() function
+    char ori[20], dup[20];
+    char *s;
+    printf("Enter your name: ");
+    gets(ori);
+    printf("Enter your duplicate name: ");
+    gets(dup);
+    s = strcpy(ori, dup);
+    printf("\nOriginal string is: %s", ori);
+    printf("\nDuplicate string is: %s", dup);
+    printf("\nValue of s is: %s", s);
+}
