503140c619569e4978e4a71529f5cefa69473304
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-26 04:57:32 +0000

    Wednesday, April 26, 2023, 10:27 AM India Standard Time

--- 26 april 2023.c
+++ 26 april 2023.c
@@ -10,3 +10,16 @@ void main()
 
 //_________________________________
 
+#include <stdio.h>
+void main()
+{
+    char name[] = ("Lovely Professional University");
+    int i = 0;
+    while (name[i] != '\0')
+    {
+        printf("%c", name[i]);
+        i++;
+    }
+}
+
+//_________________________________
