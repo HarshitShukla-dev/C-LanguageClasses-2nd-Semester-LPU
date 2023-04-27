31a9b3e3156700bade6a5efe1e5484db12ab310e
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 03:57:13 +0000

    Thursday, April 27, 2023, 9:27 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-void main()
-{
-    char str[40];
-    printf("Enter the string : ");
-    gets(str);
-    printf("Reverse of the string is %s.", strrev(str));
+void main(){
+    char str1[40],str2[40];
+    printf("Enter the string 1 : ");
+    gets(str1);
+    printf("Enter the string 2 : ");
+    gets(str2);
+    str1 = str2;
+    printf("The string 1 is %s and the string 2 is %s.", str1, str2);
 }
