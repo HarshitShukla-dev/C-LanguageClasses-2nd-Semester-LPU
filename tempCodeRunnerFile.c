5956fd454780a3660d969dabe180fd514b51d5d9
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 04:01:35 +0000

    Thursday, April 27, 2023, 9:31 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 void main(){
     char str1[40],str2[40];
+    int i = 0;
     printf("Enter the string 1 : ");
     gets(str1);
     printf("Enter the string 2 : ");
     gets(str2);
-    str1 = str2;
+    while(str1[i] != '\0'){
+        str2[i] = str1[i];
+        i++;
+    }
     printf("The string 1 is %s and the string 2 is %s.", str1, str2);
 }
