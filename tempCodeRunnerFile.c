bd28a550b4b5001d047ebed3bf87d35b03a62663
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 04:28:07 +0000

    Thursday, April 27, 2023, 9:58 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 void main()
 {
-    char str1[40], str2[40], str3[80];
-    int j = 0, i = 0, k = 0;
+    char str1[40], str2[40];
+    int i = 0;
     printf("Enter the string 1 : ");
     gets(str1);
-    printf("Enter the string 2 : ");
-    gets(str2);
     while (str1[i] != '\0')
     {
-        str3[j] = str1[i];
+        str2[i] = str1[i] + 32;
         i++;
-        j++;
     }
-    while (str2[k] != '\0')
-    {
-        str3[j] = str2[k];
-        k++;
-        j++;
-    }
-    printf("Concatanated strings is : %s", str3);
+    printf("Lowercase is %d", i);
 }
