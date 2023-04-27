9b2e7b4ebeb909707534b9dc7c5753c28a343eaa
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 04:22:57 +0000

    Thursday, April 27, 2023, 9:52 AM India Standard Time

--- 27 april 2023.c
+++ 27 april 2023.c
@@ -67,8 +67,43 @@ void main()
 
 //_________________________________
 
-// 
+//
 #include <stdio.h>
 void main()
 {
+    char str1[40], str2[40];
+    int i = 0;
+    printf("Enter the string 1 : ");
+    gets(str1);
+    while (str1[i] != '\0')
+    {
+        i++;
+    }
+    printf("Length is %d", i);
+}
+
+//_________________________________
+
+#include <stdio.h>
+void main()
+{
+    char str1[40], str2[40], str3[80];
+    int j = 0, i = 0, k = 0;
+    printf("Enter the string 1 : ");
+    gets(str1);
+    printf("Enter the string 2 : ");
+    gets(str2);
+    while (str1[i] != '\0')
+    {
+        str3[j] += str1[i];
+        i++;
+        j++;
+    }
+    while (str2[k] != '\0')
+    {
+        str3[j] += str2[k];
+        k++;
+        j++;
+    }
+    printf("Uppercase of %s is %s.");
 }
