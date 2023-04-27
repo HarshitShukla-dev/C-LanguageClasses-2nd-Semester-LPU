e84c606dbbadbdde4c7ea7cc28fb3a773441b85f
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 04:25:22 +0000

    Thursday, April 27, 2023, 9:55 AM India Standard Time

--- 27 april 2023.c
+++ 27 april 2023.c
@@ -95,15 +95,49 @@ void main()
     gets(str2);
     while (str1[i] != '\0')
     {
-        str3[j] += str1[i];
+        str3[j] = str1[i];
         i++;
         j++;
     }
     while (str2[k] != '\0')
     {
-        str3[j] += str2[k];
+        str3[j] = str2[k];
         k++;
         j++;
     }
     printf("Concatanated strings is : %s", str3);
+}
+
+//_________________________________
+
+#include <stdio.h>
+void main()
+{
+    char str1[40], str2[40];
+    int i = 0;
+    printf("Enter the string 1 : ");
+    gets(str1);
+    while (str1[i] != '\0')
+    {
+        str2[i] = str1[i] + 32;
+        i++;
+    }
+    printf("Lowercase is %d", i);
+}
+
+//_________________________________
+
+#include <stdio.h>
+void main()
+{
+    char str1[40], str2[40];
+    int i = 0;
+    printf("Enter the string 1 : ");
+    gets(str1);
+    while (str1[i] != '\0')
+    {
+        str2[i] = str1[i] - 32;
+        i++;
+    }
+    printf("Uppercase is %d", i);
 }
