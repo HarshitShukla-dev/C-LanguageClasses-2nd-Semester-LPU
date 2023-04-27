d7fa6c33bb5087df5d133db00ca989718aa72d08
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 03:46:41 +0000

    Thursday, April 27, 2023, 9:16 AM India Standard Time

--- 27 april 2023.c
+++ 27 april 2023.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 int main(){
-    char str[40];
+    char str;
     printf("Enter the string : ");
     gets(str);
     printf("Length of the string is %d.",strlen(str));
