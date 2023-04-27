8ac6ff570d76b0f025232e963e58ea807fbc6688
Hershit-shukla <hershitshukla35@gmail.com>
2023-04-27 04:27:07 +0000

    Thursday, April 27, 2023, 9:57 AM India Standard Time

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-void main(){
-    char str1[40],str2[40];
-    int i = 0;
+void main()
+{
+    char str1[40], str2[40], str3[80];
+    int j = 0, i = 0, k = 0;
     printf("Enter the string 1 : ");
     gets(str1);
     printf("Enter the string 2 : ");
     gets(str2);
-    while(str1[i] != '\0'){
-        str2[i] = str1[i];
+    while (str1[i] != '\0')
+    {
+        str3[j] = str1[i];
         i++;
+        j++;
     }
-    printf("The string 1 is %s and the string 2 is %s.", str1, str2);
+    while (str2[k] != '\0')
+    {
+        str3[j] = str2[k];
+        k++;
+        j++;
+    }
+    printf("Concatanated strings is : %s", str3);
 }
